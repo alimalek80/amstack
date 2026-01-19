b1e2fa3b0bd11d211ee4bff3e5c6c194341551f7
Ali Malek <alianta2016@gmail.com>
2026-01-19 22:47:33 +0300

    modify setting for static file in cpanel

--- amstack/settings.py
+++ amstack/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
 _static_dir = BASE_DIR / "static"
